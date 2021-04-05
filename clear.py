664edd598b7edfd52645789713057114cc8ebd56
/VS <74320425+TypeVS@users.noreply.github.com>
2021-04-05 17:26:48 -0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,6 +1,13 @@
+# Para iniciar com o windows, dê win + r e coloque "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+
 import os
 import time
 
+os.system("title Cleaner")
+
+
+delay = 60 #Coloque seu Delay aqui (segundos)
 contador = 0 
 while True:
 	os.system("cls")
@@ -9,8 +16,5 @@ while True:
 	os.system(f"del /S /Q /F C:\\Users\\vinicius\\AppData\\Local\\Temp")
 	os.system("cls")
 	contador = contador +1
-	if contador == 1:
-		print(f"[+] Arquivos Temporarios Deletados pela {contador} vez")
-	else:
-		print(f"[+] Arquivos Temporarios Deletados pela {contador} vezes")
-	time.sleep(60)
+	print(f"[+] levas de limpeza: {contador}")
+	time.sleep(delay)
