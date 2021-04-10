5aff811837bbd5dec5e72ec6a2aeaa3a44d7c1f3
/VS <74320425+TypeVS@users.noreply.github.com>
2021-04-10 16:18:01 -0300

    Sistema De update add

--- clear.py
+++ clear.py
@@ -1,9 +1,18 @@
-# Para iniciar com o windows, dê win + r e coloque "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup" e coloque seu arquivo para iniciar junto com o win
+#!/usr/bin/python
+# NOTAS: Para iniciar com o windows, dê win + r e coloque "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup" e coloque seu arquivo para iniciar junto com o win
 
 
 import os # não mude nada aqui
 import time # não mude nada aqui
 
+
+
+def upgrade():
+	os.system("cls")
+	os.system("py -m pip --upgrade")
+	
+	
+upgrade()
 os.system("title Cleaner")
 
 delay = 60 # Coloque seu Delay aqui (segundos)
