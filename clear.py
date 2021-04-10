b2fe1662d0afe5003f4652424b97a8674bffaf7a
/VS <74320425+TypeVS@users.noreply.github.com>
2021-04-10 17:01:16 -0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -9,24 +9,25 @@ import time # não mude nada aqui
 
 def upgrade():
 	os.system("cls")
-	os.system("py -m pip --upgrade")
+	print("[+] Updating")
+	os.system("py -m pip install --upgrade pip")
+	os.system("cls")
 	
 	
 upgrade()
 os.system("title Cleaner")
-delay = 60 # Coloque seu Delay aqui (segundos)
-# não mude nada daqui pra frente
-contador = 0 
-main()
 def main():
-	while True:
-	 os.system("cls")
-         os.system(f"del /S /Q /F C:\\Windows\\Temp")
-	 os.system("cls")
-	 os.system(f"del /S /Q /F C:\\Users\\vinicius\\AppData\\Local\\Temp")
-	 os.system("cls")
-	 contador = contador +1
-	 print(f"[+] levas de limpeza: {contador}")
-	 time.sleep(delay)
+	delay = 60 # Coloque seu Delay aqui (segundos)
+	contador = 0 
 
-	
+	# não mude nada daqui pra frente
+
+	while True:
+	  os.system("cls")
+	  os.system(f"del /S /Q /F C:\\Users\\vinicius\\AppData\\Local\\Temp\n")
+	  os.system(f"del /S /Q /F C:\\Windows\\Temp\n")
+	  os.system("cls")
+	  contador = contador +1
+	  print(f"[+] levas de limpeza: {contador}")
+	  time.sleep(delay)
+main()
